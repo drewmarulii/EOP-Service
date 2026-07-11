96bb9d69da6215fb79f8d0f74419b78c6421d32c
drewmarulii <drewmaruli@gmail.com>
2026-07-11 22:44:57 +0700

    FEAT: Prepare User Service Feign

--- user-service/src/main/java/com/eop/userservice/auth/controller/AuthController.java
+++ user-service/src/main/java/com/eop/userservice/auth/controller/AuthController.java
@@ -2,12 +2,11 @@ package com.eop.userservice.auth.controller;
 
 import com.eop.userservice.auth.common.LoginRequest;
 import com.eop.userservice.auth.common.LoginResponse;
+import com.eop.baseservice.common.MyInfoResponse;
+import com.eop.userservice.auth.security.SecurityUtils;
 import com.eop.userservice.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,5 +18,13 @@ public class AuthController {
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest loginRequest) {
         return authService.login(loginRequest);
+    }
+
+    @GetMapping("/my-info")
+    public MyInfoResponse myInfo() {
+        return new MyInfoResponse (
+            SecurityUtils.getUserSpeficicDetail("user_id"),
+            SecurityUtils.getUserSpeficicDetail("user_role")
+        );
     }
 }
